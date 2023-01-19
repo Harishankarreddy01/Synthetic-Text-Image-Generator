4f25267870a40de4212fc7200196ac6794959890
Harishankarreddy01 <67852753+Harishankarreddy01@users.noreply.github.com>
2023-01-19 20:23:27 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -75,3 +75,4 @@ html_title = project
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
